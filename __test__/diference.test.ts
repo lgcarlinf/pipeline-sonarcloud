acc8ce503f3d6f9e31ca6c07c26d21a17863015d
luiggy <luiggylml@gmail.com>
2024-01-18 01:49:28 -0500

    add new test

--- __test__/diference.test.ts
+++ __test__/diference.test.ts
@@ -4,7 +4,7 @@ import { difference } from '../src/utils/diference';
 
     test('should return zero when both numbers are zero', () => {
         const result = difference(0, 0);
-        expect(result).toBe(2)
+        expect(result).toBe(0)
     });
 
     test('should return the number itself when one of the numbers is zero', () => {
